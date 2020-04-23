829d734f0fdb4b2c48505136dfcbf06eac0f1523
deadro22 <44213280+deadro22@users.noreply.github.com>
2020-04-23 04:47:36 +0100

    'fr_chs

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ let io = require("socket.io")(http);
 require("./middleHandler")(app);
 require("./db/mongoConnection")();
 require("./validators/socketSetup")(io);
+app.get("*", (req, res) => {
+  res.send("Error: route not found");
+});
 
 http.listen(process.env.PORT || 80, () => {
   console.log("App started at port", process.env.CONNECTION_PORT);
